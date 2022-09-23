fe87675a6693f5d96c6a67942f5aa7bc05f57fa5
sschrs <s_ozarslan07@outlook.com>
2022-09-24 02:22:35 +0300

    Special shape creater functions were created

--- point.go
+++ point.go
@@ -1,14 +1,40 @@
 package goplotter
 
+import "image/color"
+
 type Point struct {
 	X, Y       int
 	pointShape Shape
 }
 
-func NewPoint(x, y, size int, shape Shape) *Point {
-	return &Point{
+func NewPoint(x, y int, shape Shape) Point {
+	return Point{
 		X:          x,
 		Y:          y,
 		pointShape: shape,
+	}
+}
+
+func SquarePoint(x, y, size int, clr color.Color) Point {
+	return Point{
+		X:          x,
+		Y:          y,
+		pointShape: NewRectangle(size, size, clr),
+	}
+}
+
+func CirclePoint(x, y, r int, clr color.Color) Point {
+	return Point{
+		X:          x,
+		Y:          y,
+		pointShape: NewCircle(r, clr),
+	}
+}
+
+func TrianglePoint(x, y, size int, clr color.Color) Point {
+	return Point{
+		X:          x,
+		Y:          y,
+		pointShape: NewTriangle(size, size, clr),
 	}
 }
