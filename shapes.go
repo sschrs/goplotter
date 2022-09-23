7af8a600cf995a33b707b2d171f9d7039189f34e
sschrs <s_ozarslan07@outlook.com>
2022-09-24 00:39:05 +0300

    Rectangle and Triangle functions were added

--- shapes.go
+++ shapes.go
@@ -53,3 +53,33 @@ func NewCircle(r int, shapeColor color.Color) Shape {
 	}
 	return img
 }
+
+func NewRectangle(width, height int, shapeColor color.Color) Shape {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for i := 0; i <= width; i++ {
+		for j := 0; j <= height; j++ {
+			img.Set(i, j, shapeColor)
+		}
+	}
+
+	return img
+}
+
+func NewTriangle(baseWidth, height int, shapeColor color.Color) Shape {
+	img := image.NewRGBA(image.Rect(0, 0, baseWidth, height))
+
+	positionX := int(baseWidth / 2)
+	positionY := 0
+
+	for i := 1; i <= height/2; i++ {
+		positionX -= 1
+		positionY += 1
+		img.Set(positionX, positionY, shapeColor)
+		for j := 0; j < i*2; j++ {
+			img.Set(positionX+j, positionY, shapeColor)
+		}
+
+	}
+	return img
+}
