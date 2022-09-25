1fc609b44b66f352208343ed00c0bc3d621f58ec
sschrs <s_ozarslan07@outlook.com>
2022-09-25 15:29:41 +0300

    NewLegendItem and NewLegend functions are written

--- legend.go
+++ legend.go
@@ -17,6 +17,27 @@ type LegendItem struct {
 type Legend struct {
 	Title                       string
 	TitleColor, BackgroundColor color.Color
-	Items                       []LegendItem
+	Items                       []*LegendItem
 	Width, Height, X, Y         int
+}
+
+func NewLegendItem(text string, symbol Shape) *LegendItem {
+	return &LegendItem{
+		Text:      text,
+		TextColor: defaultTextColor,
+		Symbol:    symbol,
+	}
+}
+
+func NewLegend(title string, items []*LegendItem, width, height, x, y int) *Legend {
+	return &Legend{
+		Title:           title,
+		TitleColor:      defaultTitleColor,
+		BackgroundColor: defaultBackgroundColor,
+		Items:           items,
+		Width:           width,
+		Height:          height,
+		X:               x,
+		Y:               y,
+	}
 }
