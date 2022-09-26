80c6f5c50cb6240fae00e9915f330493182b4e9f
sschrs <s_ozarslan07@outlook.com>
2022-09-27 00:43:00 +0300

    created NewScatterPlot function

--- plotter/scatter.go
+++ plotter/scatter.go
@@ -1,10 +1,31 @@
 package plotter
 
-import "image/color"
+import (
+	"fmt"
+	"image/color"
+)
 
 type ScatterPlotter struct {
 	XValues, YValues                                     []string
 	Title, XLabel, YLabel                                string
 	BackroundColor, TitleColor, XLabelColor, YLabelColor color.Color
 	AddLegend                                            bool
+}
+
+func NewScatterPlot(x []interface{}, y []float64) *ScatterPlotter {
+	var xLabels []string
+	var yLabels []string
+
+	for _, xValue := range x {
+		xLabels = append(xLabels, fmt.Sprintf("%v", xValue))
+	}
+
+	for _, yValue := range y {
+		yLabels = append(yLabels, fmt.Sprintf("%f", yValue))
+	}
+
+	return &ScatterPlotter{
+		XValues: xLabels,
+		YValues: yLabels,
+	}
 }
