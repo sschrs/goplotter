d92c5905d7cbb614a56e0a45fc496adde1c621ad
sschrs <s_ozarslan07@outlook.com>
2022-09-27 13:55:30 +0300

    created MinMax function

---
+++ plotter/utils.go
@@ -0,0 +1,15 @@
+package plotter
+
+func MinMax(array []float64) (float64, float64) {
+	var max float64 = array[0]
+	var min float64 = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
